efecdf98228b380ce3f7168d56645344d469f55a
arsil suthat <98098826+arsil2029@users.noreply.github.com>
2022-02-26 04:23:55 -1200

    Add files via upload

---
+++ arseel.js
@@ -0,0 +1,41 @@
+function show() {
+    nav = document.getElementById('navbar');
+    if (nav.style.display == "none")
+     {
+        nav.style.display = "block"
+        
+    }
+    else{
+        nav.style.display = "none"
+    }  
+   
+
+ }
+// setTimeout(show,2000) ;
+
+// setTimeout(show=() => {
+//     nav = document.getElementById('navbar');
+//     if (nav.style.display == "none")
+//      {
+//         nav.style.display = "block"
+        
+//     }
+//     else{
+//         nav.style.display = "none"
+//     }  
+   
+    
+// }, 10000);
+
+// show.addEventListener('click'function(){
+
+//     nav = document.getElementById('navbar');
+//     if (nav.style.display == "none")
+//      {
+//         nav.style.display = "block"
+        
+//     }
+//     else{
+//         nav.style.display = "none"
+//     } 
+// })
